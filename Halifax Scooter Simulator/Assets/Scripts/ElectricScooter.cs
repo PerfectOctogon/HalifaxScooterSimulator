6f56e086f4ad1c78a9540080228171ae54ea9e9f
PerfectOctogon <blazecoc777@gmail.com>
2024-04-28 20:52:56 -0300

    Needs wheel rotation bug fix

---
+++ Halifax Scooter Simulator/Assets/Scripts/ElectricScooter.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class ElectricScooter : MonoBehaviour
+{
+    public WheelCollider frontCollider, rearCollider;
+
+    public Transform frontWheel, backWheel;
+
+    public float steeringAngle = 60;
+
+    public float motorPower = 25;
+
+    public float steering;
+
+    private float horizontal;
+
+    private float vertical;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        InputManager();
+        Drive();
+        Steer();
+        UpdateWheelPositions(frontCollider, frontWheel);
+        UpdateWheelPositions(rearCollider, backWheel);
+    }
+
+    void InputManager()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    void Drive()
+    {
+        rearCollider.motorTorque = vertical * motorPower;
+    }
+
+    void Steer()
+    {
+        steering = steeringAngle * horizontal;
+        frontCollider.steerAngle = steering;
+    }
+
+    void UpdateWheelPositions(WheelCollider wc, Transform t)
+    {
+        Vector3 pos = t.localPosition;
+        Quaternion rot = t.rotation;
+        
+        wc.GetWorldPose(out pos, out rot);
+        t.position = pos;
+        t.localRotation = rot;
+    }
+}
